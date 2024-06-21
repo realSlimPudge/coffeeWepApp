6949b1bf2c75680ecbb1dce56de45b220c360b9d
realSlimPudge <164773060+realSlimPudge@users.noreply.github.com>
2024-06-21 11:36:59 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,9 @@ const countGoodsHtml = document.getElementById('countGoods');
 const countPlus = document.getElementById('countPlus');
 const countMinus = document.getElementById('countMinus');
 const openModal = document.getElementById('card');
+const modalCart = document.getElementById('shopCart')
+const openCart = document.getElementById('openCart')
+const closeCart = document.getElementById('closeCart')
 
 let countGoods = countGoodsHtml;
 
@@ -36,9 +39,19 @@ category3.addEventListener('click', () => {
   category4.classList.remove('category_on_active');
 });
 
-category4.addEventListener('click', () => {
-  category4.classList.add('category_on_active');
-  category2.classList.remove('category_on_active');
-  category3.classList.remove('category_on_active');
-  category.classList.remove('category_on_active');
-});
+// category4.addEventListener('click', () => {
+//   category4.classList.add('category_on_active');
+//   category2.classList.remove('category_on_active');
+//   category3.classList.remove('category_on_active');
+//   category.classList.remove('category_on_active');
+// });
+
+
+openCart.addEventListener('click',function () {
+  modalCart.showModal()
+})
+
+
+closeCart.addEventListener('click',()=>{
+  modalCart.close();
+})
